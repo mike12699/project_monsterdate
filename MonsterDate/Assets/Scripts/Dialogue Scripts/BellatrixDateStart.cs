f64884412dfeab48b0bd3678b7be93fa6cd9a703
mike12699 <mariomania85@gmail.com>
2022-03-25 11:34:33 -0500

    Readjusted the confirm panel over the text box

--- MonsterDate/Assets/Scripts/Dialogue Scripts/BellatrixDateStart.cs
+++ MonsterDate/Assets/Scripts/Dialogue Scripts/BellatrixDateStart.cs
@@ -12,7 +12,7 @@ public class BellatrixDateStart : MonoBehaviour
     {
         var dialogueTexts = new List<DialogData>();
 
-        dialogueTexts.Add(new DialogData("/speed:0.03//emote:Shocked/A LICHYARD? MY, MY, THIS PARAMOUR-TO-BE HAS AN AFFINITY FOR THE UNDEAD, THEN?", "Askarios"));
+        dialogueTexts.Add(new DialogData("/speed:0.03//emote:Shocked//sound:haha/A LICHYARD? MY, MY, THIS PARAMOUR-TO-BE HAS AN AFFINITY FOR THE UNDEAD, THEN?", "Askarios"));
         dialogueTexts.Add(new DialogData("/speed:0.03/Well, all I know is that they come here often.", "Skully"));
         dialogueTexts.Add(new DialogData("/speed:0.03/PERHAPS THEY ARE A LICH THEMSELVES! I WOULD SO ADORE TO SPOT ANOTHER OF MY KIND AND CRAFT.", "Askarios"));
         dialogueTexts.Add(new DialogData("/speed:0.03//emote:Sad/Eh... /wait:0.5/ya didn't look at the picture I gave you, huh, boss?", "Skully"));
