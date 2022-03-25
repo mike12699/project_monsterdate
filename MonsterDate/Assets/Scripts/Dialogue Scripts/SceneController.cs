590ae8612bb89096b143a585c938dc79c9a27861
Gearshead19 <64817807+Gearshead19@users.noreply.github.com>
2022-03-24 21:54:30 -0500

    Game set up to build

--- MonsterDate/Assets/Scripts/Dialogue Scripts/SceneController.cs
+++ MonsterDate/Assets/Scripts/Dialogue Scripts/SceneController.cs
@@ -19,6 +19,10 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene("BugGirlPuzzle");
     }
+    public void LoadRules()
+    {
+        SceneManager.LoadScene("RuleScreen");
+    }
 
     public void LoadMothmanMatch3()
     {
